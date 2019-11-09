4a4d1c2b6dc56e592c3ee31e3e985e76284633e1
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2019-11-10 07:49:03 +0900

    no message

--- gitf/settings.py
+++ gitf/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from corsheaders.middleware import CorsMiddleware
+# from corsheaders.middleware import CorsMiddleware
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddlewaredjango.middleware.common',
     'corsheaders.middleware.CorsMiddleware ',
 ]
 
