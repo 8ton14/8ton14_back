235905f7997ab18d416f14c0079305ce04022d53
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2019-11-10 06:33:42 +0900

    gp

--- gitf/wsgi.py
+++ gitf/wsgi.py
@@ -15,8 +15,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gitf.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application) 
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application) 
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gitf.settings")
